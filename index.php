6a79f537bfa2a52ad61d15b9f75647d52bd22176
Muhammad Ridwan <bobar717@gmail.com>
2023-02-25 21:34:16 +0700

    add list

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 <body>
   <h1>hello</h1>
   <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, praesentium sed explicabo laudantium doloribus ex, at aliquid reiciendis non excepturi dolores repellendus perspiciatis ea esse nulla quaerat, autem sapiente modi!</p>
+  <ul>satu</ul>
 </body>
 </html>
